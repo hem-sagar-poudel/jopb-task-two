16f1962f529c5d04027cc83a0ebe038ec369d795
Hemsagar Poudel <sagarpoudel17@gmail.com>
2023-07-29 06:54:45 +0545

    second push

--- store/zustand/news.js
+++ store/zustand/news.js
@@ -1,4 +1,5 @@
 import {getNews} from "api/new";
+import {getLocalStorage, setLocalStorage} from "utils/storageUtil";
 import {create} from "zustand";
 
 export const useNewsStore = create((set) => ({
@@ -7,12 +8,17 @@ export const useNewsStore = create((set) => ({
   fetchData: async (value) => {
     console.log(value);
     try {
-      const res = await getNews(value);
-      console.log(res);
-      if (res.status == "ok") {
+      if (!getLocalStorage(value)) {
+        const res = await getNews(value);
+        console.log(res);
+        if (res.status == "ok") {
+          setLocalStorage(value, res.articles);
+          return set((state) => ({data: res.articles}));
+        }
         return set((state) => ({data: res.articles}));
+      } else {
+        return set((state) => ({data: getLocalStorage(value)}));
       }
-      return set((state) => ({data: res.articles}));
     } catch (error) {
       return set((state) => ({data: res.articles}));
     }
